d5b8392df3d885778a3de4ad7123ba7397d5bb18
Ashish Srivastava <ashishsrivastava7007@gmail.com>
2024-10-12 20:43:11 +0530

    changed logos

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,10 +1,13 @@
-import DashboardIcon from '@mui/icons-material/Dashboard';
+import DirectionsBoatIcon from '@mui/icons-material/DirectionsBoat';
+import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
 export const countAllStatuses = (shipments) => {
     const statuses = [
-        { id: 1, name: "In Transit", icon: DashboardIcon },
-        { id: 2, name: "Pending", icon: DashboardIcon },
-        { id: 3, name: "Delayed", icon: DashboardIcon },
-        { id: 4, name: "Delivered", icon: DashboardIcon}
+        { id: 1, name: "In Transit", icon: DirectionsBoatIcon },
+        { id: 2, name: "Pending", icon: HourglassBottomIcon },
+        { id: 3, name: "Delayed", icon: WatchLaterIcon },
+        { id: 4, name: "Delivered", icon: LocalShippingIcon}
     ];
 
     const counts = statuses.map(status => {
